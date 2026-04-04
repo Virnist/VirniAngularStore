992ac2bd23743997531b79f0922ad60fe721c485
Andrii Fedoriv <andrijfedoriv@email.com>
2026-04-04 17:19:53 +0300

    update gitignore

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  // Вставте ваші дані між лапками:
+  youtubeApiKey: 'AIzaSyAdu2X-yY6fml_RLL9BdWb4l74pSKAOLtw',
+  youtubeChannelId: 'UCNilfw7uSJVDhUcLLYcD_Cw'
+};
